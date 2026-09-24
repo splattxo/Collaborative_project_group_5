8e3bd6b39ed1a45295908618a76928d04ab9953f
splattxo <spenser.platt@gmail.com>
2026-09-24 10:05:12 -0700

    Initial commit

---
+++ plots.R
@@ -0,0 +1,20 @@
+### This is code to produce a scatter plot on the `mtcars` (motortrends) dataset
+
+# load required packages
+library(dplyr)
+library(ggplot2)
+
+# load in data set
+data(mtcars)
+
+# create a scatterplot
+mtcars %>% 
+    mutate(cyl = as.factor(cyl)) %>%
+    ggplot(aes(x = hp, y = mpg, color = cyl, shape = cyl)) + 
+    geom_point() + 
+    scale_color_brewer(palette = "Set2")  +
+    xlab("Horsepower") + 
+    ylab("Miles Per Gallon") +
+    labs(color = "Cylinders", shape = "Cylinders") +
+    theme_minimal() + 
+    ggtitle("CHANGE THIS TITLE") #update this line
